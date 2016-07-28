aa2561d283969061ba94b2f94cce32ab76186c86
CircleCI <circleci@cureapp.jp>
2016-07-28 17:51:55 +0000

    v0.0.2

--- test/test-development.js
+++
@@ -1,7 +0,0 @@
-const config = require('..')
-const assert = require('assert')
-
-assert.deepEqual(config, {
-    BASE_URL: 'http://localhost:8000/api',
-    DB_NAME: 'local'
-})
--- test/test-staging.js
+++
@@ -1,7 +0,0 @@
-const config = require('..')
-const assert = require('assert')
-
-assert.deepEqual(config, {
-    BASE_URL: 'http://staging.example.com/api',
-    DB_NAME: 'staging'
-})
